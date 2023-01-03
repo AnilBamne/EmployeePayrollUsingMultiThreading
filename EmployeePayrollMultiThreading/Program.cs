19758fed22412560dd4e44ec72931ca7496be6d5
AnilBamne <anilbamne20@gmail.com>
2023-01-03 21:30:33 +0530

    Adding:Welcome message

---
+++ EmployeePayrollMultiThreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollMultiThreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee payroll using MultiThreading");
+        }
+    }
+}
